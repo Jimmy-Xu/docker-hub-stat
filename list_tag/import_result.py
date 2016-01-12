e50dfdd7bbfcf3012d9ddf635bdbf14101406f40
Jimmy Xu <xjimmyshcn@gmail.com>
2016-01-12 12:47:34 +0800

    [master] support import list_tag result to mongo

---
+++ list_tag/import_result.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import time
+import os
+import sys
+import json
+import argparse
+
+import dateutil.parser
+from prettytable import PrettyTable
+from pymongo import MongoClient
+
+g_result=[]
+
+def parse_tag(base_dir, user_dir, repo_dir, page_file):
+    global g_result
+    f="{0}/{1}/{2}/{3}".format(base_dir,user_dir,repo_dir,page_file)
+    #print "read data from file: {0}".format(f)
+    with open(f) as data_file:
+        data = json.load(data_file)
+    repo_name = repo_dir
+
+    if "results" not in data:
+        import pdb;pdb.set_trace()
+        print "wrong data format in file: {0}".format(f)
+    else:
+        for item in data["results"]:
+            item["repo_name"] = repo_name
+            item["namspace"] = user_dir
+            g_result.append(item)
+
+def read_tag():
+    IN_DIR = "list_result"
+    i = 1
+
+    ##print "\nlist dir under dir: {0}".format(IN_DIR)
+    #level 1(user dir)
+    for user_dir in os.listdir(IN_DIR):
+        user_path = os.path.join(IN_DIR, user_dir)
+        if os.path.isdir(user_path):
+            ##print "\tlist file under dir: {0}".format(user_path)
+            #level 2(repo dir)
+            for repo_dir in os.listdir(user_path):
+                try:
+                    page_path = os.path.join(user_path, repo_dir)
+                    if os.path.isdir(page_path):
+                        ##print "\t\tlist file under dir: {0}".format(page_path)
+                        #level 3(page file)
+                        for page_file in os.listdir(page_path):
+                            try:
+                                page_path = os.path.join(page_path, page_file)
+                                if os.path.isfile(page_path):
+                                    parse_tag(IN_DIR, user_dir, repo_dir, page_file)
+                            except:
+                                continue
+                except:
+                    continue
+        i = i + 1
+
+def import_tag():
+    global g_result
+
+    if g_result:
+        client = MongoClient('localhost', 27017)
+        db = client.docker
+        print "drop old data in tag collection"
+        db.image_tag.drop()
+        db.create_collection("image_tag")
+        print "start_time:{0}".format(time.strftime('%Y-%m-%d %H:%M:%S'))
+        db.image_tag.insert(g_result)
+        print "end_time:{0}".format(time.strftime('%Y-%m-%d %H:%M:%S'))
+    else:
+        print "There is no data to import"
+
+#### main #####
+read_tag()
+import_tag()
---
+++ mongo/run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#image name
+NAMESPACE="xjimmyshcn"
+REPO_NAME="mongo"
+IMAGE_TAG="3.2"
+IMAGE_NAME=${NAMESPACE}/${REPO_NAME}:${IMAGE_TAG}
+
+#container and image name
+CONTAINER_NAME="mongo"
+
+#volume
+LOCAL_DB_DIR=`pwd`/db
+SRC_DATA_DIR=`pwd`/..
+
+#port
+LOCAL_PORT="27017"
+
+############################################################
+#ensure image exist
+docker images ${IMAGE_NAME} | grep " ${IMAGE_TAG} "
+if [ $? -ne 0 ];then
+  docker pull ${IMAGE_NAME}
+  if [ $? -ne 0 ];then
+    echo "pull docker image ${IMAGE_NAME} failed, please try again"
+    exit 1
+  fi
+fi
+
+############################################################
+#build image
+docker build -t ${IMAGE_NAME} .
+
+############################################################
+#ensure old container not exist
+docker ps -a | grep " mongo$"
+if [ $? -eq 0 ];then
+  docker rm -f ${CONTAINER_NAME}
+fi
+
+############################################################
+#ensure db dir exist
+if [ ! -d ${LOCAL_DB_DIR} ];then
+  mkdir -p ${LOCAL_DB_DIR}
+fi
+
+############################################################
+#start new container
+docker run --name ${CONTAINER_NAME} -d -p ${LOCAL_PORT}:27017 -v ${LOCAL_DB_DIR}:/data/db/ -v ${SRC_DATA_DIR}:/data/source ${IMAGE_NAME}
+
+cat <<EOF
+
+-------------------------------------------
+to enter container:
+  docker exec -it ${CONTAINER_NAME} /bin/bash
+
+to import list_tag result:
+  docker exec -it ${CONTAINER_NAME} bash -c "cd /data/source/list_tag;./import_result.py"
+
+EOF
