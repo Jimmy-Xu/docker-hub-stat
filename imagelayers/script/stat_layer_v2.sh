d579385d2729bdd96811ddc4d70a34b565f8af72
Jimmy Xu <xjimmyshcn@gmail.com>
2016-03-22 23:08:47 +0800

    [master] imagelayers: add cache for token

--- imagelayers/script/stat_layer_v2.sh
+++ imagelayers/script/stat_layer_v2.sh
@@ -5,12 +5,6 @@ set -e
 # https://raw.githubusercontent.com/docker/docker/6bf8844f1179108b9fabd271a655bf9eaaf1ee8c/contrib/download-frozen-image-v2.sh
 ###############################################################################################################################
 
-# hello-world                      latest              ef872312fe1b        3 months ago        910 B
-# hello-world                      latest              ef872312fe1bbc5e05aae626791a47ee9b032efa8f3bda39cc0be7b56bfe59b9   3 months ago        910 B
-
-# debian                           latest              f6fab3b798be        10 weeks ago        85.1 MB
-# debian                           latest              f6fab3b798be3174f45aa1eb731f8182705555f89c9026d8c1ef230cbf8301dd   10 weeks ago        85.1 MB
-
 if ! command -v curl &> /dev/null; then
 	echo >&2 'error: "curl" not found!'
 	exit 1
@@ -19,6 +13,7 @@ fi
 usage() {
 	echo "usage: $0 image[:tag][@digest] ..."
 	echo "       $0 hello-world:latest@sha256:8be990ef2aeb16dbcb9271ddfe2610fa6658d13f6dfb8bc72074cc1ca36966a7"
+  echo "       $0 ubuntu:latest alpine:latest ubuntu:14.04 alpine:3.3"
 	[ -z "$1" ] || exit "$1"
 }
 
@@ -26,6 +21,9 @@ if [ $# -eq 0 ];then
   usage 1 >&2
 fi
 
+declare -A dic_token
+
+#same repo use the same token
 while [ $# -gt 0 ]; do
 	imageTag="$1"
 	shift
@@ -39,7 +37,15 @@ while [ $# -gt 0 ]; do
 		image="library/$image"
 	fi
 
-	token="$(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$image:pull" | jq --raw-output .token)"
+  # fetch token in cache
+  token=${dic_token[$image]}
+  if [ -z $token ];then
+    #fetch new token
+  	token="$(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$image:pull" | jq --raw-output .token)"
+    dic_token[$image]=$token
+  # else
+  #   echo -n "[*]"
+  fi
 
 	manifestJson="$(curl -sSL -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/$image/manifests/$digest")"
 	if [ "${manifestJson:0:1}" != '{' ]; then
